05a57140bd36ba8d137a38efc6d3ee5bcbb3faa0
Abdan Hurmuzi <abdan.idza2345@gmail.com>
2024-02-25 00:30:43 +0700

    Update pushToLocalStorage.js

--- script/pushToLocalStorage.js
+++ script/pushToLocalStorage.js
@@ -4,10 +4,5 @@ saveLocalButton.addEventListener("click", async (e) => {
   let raw_text = document.getElementById("vlink").value;
   raw_text = raw_text.trim();
   const klist = raw_text.split("\n");
-
-  try {
-    localStorage.setItem("klist", klist);
-  } catch (error) {
-    alert("Cannot set localStorage item!");
-  }
+  localStorage.setItem("klist", klist);
 });
