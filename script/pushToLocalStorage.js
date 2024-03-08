9c52c27b623b2e1b2af24db365b51816bfb2db8c
Abdan Hurmuzi <abdan.idza2345@gmail.com>
2024-03-08 19:37:12 +0700

    add validation

--- script/pushToLocalStorage.js
+++ script/pushToLocalStorage.js
@@ -3,6 +3,13 @@ const saveLocalButton = document.getElementById("generateReport-button");
 saveLocalButton.addEventListener("click", async (e) => {
   let raw_text = document.getElementById("vlink").value;
   raw_text = raw_text.trim();
-  const klist = raw_text.split("\n");
-  localStorage.setItem("klist", klist);
+  if (raw_text == "") {
+    alert("Please fill out the blank space!");
+    return false;
+  } else {
+    const klist = raw_text.split("\n");
+    localStorage.setItem("klist", klist);
+    window.location.href = "/generateReports.html";
+    return true;
+  }
 });
